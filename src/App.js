2b49f367001a50e23c399e1de04cf0db3b925cf8
spyer <sundayomoladee11@gmail.com>
2024-11-05 13:52:06 +0100

    play muisic section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Hero from './components/Hero';
 import About from './components/About';
+import LastesAlbum from './components/LastesAlbum';
 // import Subscription from './components/Subscription';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <LastesAlbum/>
       {/* <Subscription/> */}
     </div>
     
